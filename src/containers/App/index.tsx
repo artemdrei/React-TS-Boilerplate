17554d5a1bf92044f8add473b608a74d00964ba0
Artem Drei <drei.artem@pdffiller.team>
2020-09-29 18:59:10 +0300

    Remove Private Route

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Switch, Redirect } from 'react-router-dom';
+import { Switch, Redirect, Route } from 'react-router-dom';
 
-import PrivateRoute from '@root/components/PrivateRoute';
 import Navigation from '@root/containers/Pages/Common/Navigation';
 import Todos from '@root/containers/Pages/Todos';
 import Photos from '@root/containers/Pages/Photos';
@@ -9,18 +8,14 @@ import Photos from '@root/containers/Pages/Photos';
 import s from './styles.scss';
 
 const App = () => {
-  const isLoggedIn = true;
-
   return (
     <div className={s.app}>
-      <div className={s.container}>
-        <Navigation />
-        <Switch>
-          <PrivateRoute path="/todos" condition={isLoggedIn} component={Todos} redirect="/" />
-          <PrivateRoute path="/photos" condition={isLoggedIn} component={Photos} redirect="/" />
-          <Redirect to="/todos" />
-        </Switch>
-      </div>
+      <Navigation />
+      <Switch>
+        <Route path="/todos" component={Todos} />
+        <Route path="/photos" component={Photos} />
+        <Redirect to="/todos" />
+      </Switch>
     </div>
   );
 };
